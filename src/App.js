b44e648ee7a21dfdf36174406195cd4f181b814b
Gary Tate <me@garytate.co.uk>
2020-09-01 22:08:33 +0100

    switched to react

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Gary Tate</h1>
+        <a href="https://github.com/garytate/"><h2>Github</h2></a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
