4f2d21fdc7f6a6aa39bc6e749d4dde1371a6c033
Gary Tate <me@garytate.co.uk>
2020-09-01 23:10:38 +0100

    Updates

---
+++ precache-manifest.6f23b5bc512af2f598f32251fa51def2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a78b783d81dd8a18181fc1a09786659a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "edfb362c02148520cdbc",
+    "url": "/static/css/main.8e552089.chunk.css"
+  },
+  {
+    "revision": "c7dde3cde998b1e69980",
+    "url": "/static/js/2.bebf0ff7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.bebf0ff7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edfb362c02148520cdbc",
+    "url": "/static/js/main.7f8a96e0.chunk.js"
+  },
+  {
+    "revision": "163433314168cd0db6dc",
+    "url": "/static/js/runtime-main.8ddad261.js"
+  }
+]);
